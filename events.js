f92a71e923badf81d2d94574cd0d7751a73690e7
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-28 12:06:46 -0600

    got h1 and input box working

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 let target = document.getElementById("output-target");
+let pageTitle = document.getElementById("page-title");
 
 // create a function that prints targeted area
 function print(prams) {
@@ -9,5 +10,23 @@ function print(prams) {
 let targetSection = document.querySelector("article");
 // event listners to print new text
 targetSection.addEventListener("click", () =>{
-    print(`You clicked on the ${event.target.innerHTML} section`)
+    print(`You clicked on the ${event.target.innerHTML} section`);
+});
+
+// target h1 
+let h1Target = document.querySelector("h1");
+// event listner for mouseover
+h1Target.addEventListener("mouseover", () => {
+    print(`You moved your mouse over the header`);
+});
+// event listner for mouseout 
+h1Target.addEventListener("mouseout", () =>{
+    print(`You left me!!`)
+});
+
+// target input feild 
+let inputTarget = document.getElementById("keypress-input")
+// event listner for mirror text
+inputTarget.addEventListener("input", () =>{
+    print(event.target.vaule);
 });
